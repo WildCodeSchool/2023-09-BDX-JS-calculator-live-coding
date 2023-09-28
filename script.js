e7e1ada599e0b306372e43331f03e686e26bcbf1
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2023-09-28 12:36:43 +0200

    work on display result function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('calculatrice wild code scholl 09 23 Ò.ó');
+console.log("calculatrice wild code scholl 09 23 Ò.ó");
+const displayResult = (nombre1, operateur, nombre2, calculate = false) => {
+  const resultSpan = document.querySelector(".result-txt");
+  resultSpan.innerHTML = `${nombre1} ${operateur} ${nombre2}`;
+};
+displayResult(1, "m", 1);
